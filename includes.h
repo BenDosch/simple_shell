699ec00bbbf1c02c803afe2fa3d454431b18f03a
Selidex <justindparnell@yahoo.com>
2020-11-13 20:29:31 +0000

    Adding initial header files

---
+++ includes.h
@@ -0,0 +1,15 @@
+#ifndef INCLUDES_H
+#define INCLUDES_H
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdarg.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include "prototype.h"
+
+#endif /* InCLUDES_H */
---
+++ prototype.h
@@ -0,0 +1,7 @@
+#ifndef PROTOTYPE_H
+#define PROTOTYPE_H
+
+void free_d_ptr(char **ptr);
+char **sherlock(char *str, const char *delim);
+
+#endif /* PROTOYPE_H */
