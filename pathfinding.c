a53991eb71bce1e28d6c712c76fab6a11d271066
Ben Dosch <Benjamindosch@gmail.com>
2020-11-15 22:47:41 +0000

    pathfinding.c set up, needs string functions

---
+++ pathfinding.c
@@ -0,0 +1,39 @@
+#include "includes.h"
+
+/**
+ * get_file_path - get full file path of
+ * @filename: filename to check for full path of
+ * Return: full path of file name
+ */
+
+char *get_file_path(char *filename)
+{
+	DIR *directory;
+	struct dirent *dint;
+	int i, status;
+	char **paths = sherlock(_getenv("PATH"));
+
+	for(i = 0; paths[i], i++)
+	{
+		directory = open(paths[i]);
+		while(dint = readdir(directory))
+		{
+			if (_strcmp(dint->d_name, '.') == 0 || _strcmp(dint->d_name, '..') == 0)
+				continue;
+			else if (_strcmp(dint->d_name, filename) == 0)
+			{
+				filename = _strcat(dint->dname, filename); /*maybe have a temp then change it after checks*/
+				if (filename == /*_strcmp fail*/)
+				{
+					/* error */
+				}
+				close(directory);
+				free_d_ptr(paths)
+				return (filename);
+			}
+		}
+		close(directory);
+	}
+	free_d_ptr(paths);
+	return (NULL);
+}
