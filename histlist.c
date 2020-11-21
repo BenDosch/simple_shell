002767d1d7f4746372d852591e45a43f78660b27
Selidex <justindparnell@yahoo.com>
2020-11-21 07:36:34 +0000

    adding helper file for History Builtin

---
+++ histlist.c
@@ -0,0 +1,10 @@
+/* make list */
+
+/* free list */
+
+/* print list goes in history built in? */
+
+/* run command in history using format !X
+ *should handle !X (where x is the command number),
+ * "*" where * is a pattern
+ *and !! which runs the last command given */
