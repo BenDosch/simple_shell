f421c30c4a3f6c7f689c578b507fc0f1640e0a0d
Selidex <justindparnell@yahoo.com>
2020-11-21 11:58:03 +0000

    Betty compliance

--- histlist.c
+++ histlist.c
@@ -1,10 +1,18 @@
-/* make list */
+/*
+ *make list
+*/
 
-/* free list */
+/*
+ *free list
+ */
 
-/* print list goes in history built in? */
+/*
+ *print list goes in history built in?
+ */
 
-/* run command in history using format !X
+/*
+ *run command in history using format !X
  *should handle !X (where x is the command number),
  * "*" where * is a pattern
- *and !! which runs the last command given */
+ *and !! which runs the last command given
+ */
