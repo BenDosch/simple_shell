4f55064a230d4c9a85b7a732e578cd6416460659
Selidex <justindparnell@yahoo.com>
2020-11-21 11:29:29 +0000

    Fixed memory leaks made by exit builtin

--- builtin1.c
+++ builtin1.c
@@ -1,11 +1,22 @@
 #include "includes.h"
 
-/* exit built-in */
+/**
+ *sl_exit - shell built in Exit
+ *@commands: an array of command arguments
+ *@buffer: a buffer for reading in commands, passed
+ *here to be freed
+ *Return: exit status
+ */
+
 int sl_exit(char **commands, char *buffer)
 {
 	free(buffer);
 	if (commands[1])
+	{
+		free_d_ptr(commands);
 		exit(1);
+	}
+	free_d_ptr(commands);
 	exit(0);
 }
 /* env built-in */
