5b1c1e1dee0a0d455cd1a5681ff8fbf432e238cf
Ben Dosch <Benjamindosch@gmail.com>
2020-11-22 01:28:58 +0000

    work for env

--- builtin1.c
+++ builtin1.c
@@ -8,7 +8,32 @@ int sl_exit(char **commands, char *buffer)
 		exit(1);
 	exit(0);
 }
+
 /* env built-in */
+/**
+ * sl_env - prints current enviroment
+ * @commands:
+ * @buffer:
+ * Return:
+ */
+int sl_env(char **commands, char *buffer)
+{
+	int i, j;
+	unsigned int varsize;
+
+	printf("in function\n");
+	for (i = 0; __environ[i]; i++)
+	{
+		if (i > 0)
+			write(1, "\n", 1);
+		printf("in loop\n");
+	       	varsize = strlen(__environ[i]);
+		write(1, __environ[i], varsize);
+	}
+	free(buffer);
+	return (1);
+}
+
 
 /* setenv built-in */
 
