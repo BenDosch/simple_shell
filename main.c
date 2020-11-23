3678b40a48ec95ba64e94089c95e028725b1924b
Selidex <justindparnell@yahoo.com>
2020-11-23 20:48:04 +0000

    MADNESS

--- main.c
+++ main.c
@@ -22,12 +22,19 @@ void sigintHandler(int sig_num)
 int runexe(char **commands)
 {
 	pid_t child;
+	struct stat st;
 	int status;
 	char *no_dir = commands[0];
 
+	if (commands[0][0] == '/')
+	{
+		if (stat(commands[0], &st) != 0)
+			write(1, "No such file or directory\n", 26);
+	}
+	else
 	{
 		commands[0] = get_file_path(commands[0]);
-		if (access(no_dir, status) == 0 && commands[0] == NULL)
+		if (stat(no_dir, &st) == 0 && commands[0] == NULL)
 		{
 			free(commands[0]);
 			commands[0] = no_dir;
@@ -35,17 +42,21 @@ int runexe(char **commands)
 		else
 			free(no_dir);
 	}
-	if (commands[0] != NULL)
+	if (commands[0] == NULL)
+		write(1, "No such file or directory\n", 26);
+	else
 	{       child = fork();
 		if (child == -1)
 		{       write(1, "Fork Failed\n", 12);
 			return (1);             }
 		else if (child == 0)
 		{
-			execve(commands[0], commands, __environ);
+			execve(commands[0], commands, NULL);
 		}
 		else
 			wait(&status);  }
+	(void)st;
+	(void)status;
 	return (0);
 }
 
@@ -78,29 +89,6 @@ int (*runcommand(char **commands, char *buffer))(char **, char *)
 }
 
 /**
- *cnf - prints error message if command not found
- *@pn: program name (av[0])
- *@cn: command name (commands[0])
- *@i: command number
- *Return: void no return
- */
-
-void cnf(char *pn, char *cn, int i)
-{
-
-	char *stri = "nil";
-
-	stri = _itoa(stri, i);
-	write(STDERR_FILENO, pn, _strlen(pn));
-	write(STDERR_FILENO, ": ", 2);
-	write(STDERR_FILENO, stri, _strlen(stri));
-	write(STDERR_FILENO, ": ", 2);
-	write(STDERR_FILENO, cn, _strlen(cn));
-	write(STDERR_FILENO, ": not found\n", 12);
-	free(stri);
-}
-
-/**
  *main - entry point for the shell Sherlock
  *@ac: number of arguments
  *@av: array of arguments
@@ -110,9 +98,9 @@ void cnf(char *pn, char *cn, int i)
 
 int main(int ac, char **av, char **env)
 {
-	int fb = 0, status, i = 1;
+	int fb = 0, status;
 	size_t bufsize = 1024;
-	char *buffer, **commands, *pn = av[0], *cn;
+	char *buffer, **commands;
 	struct stat st;
 
 	signal(SIGINT, sigintHandler);
@@ -120,30 +108,34 @@ int main(int ac, char **av, char **env)
 	{
 		buffer = (char *)malloc(bufsize * sizeof(char));
 		if (buffer == NULL)
+		{
+			write(1, "Error: No Memory\n", 17);
 			exit(1);
+		}
 		if (isatty(STDIN_FILENO))
 			write(1, "$ ", 2);
 		fb = getline(&buffer, &bufsize, stdin);
 		if (fb == EOF)
-		{	free(buffer);
-			exit(0); }
+		{
+			write(1, "\n", 1);
+			free(buffer);
+			exit(0);
+		}
 		buffer = watson(buffer);
 		if (*buffer == '\n')
+		{
 			free(buffer);
+		}
 		else
 		{
 			commands = sherlock(buffer, " ");
-			cn = _strdup(commands[0]);
 			(*runcommand(commands, buffer))(commands, buffer);
-			if (commands[0] == NULL)
-				cnf(pn, cn, i);
-			free(cn);
 			free(buffer);
 			free_d_ptr(commands);
 		}
-		i++;
 	}
 	(void)ac;
+	(void)av;
 	(void)env;
 	(void)status;
 	(void)st;
